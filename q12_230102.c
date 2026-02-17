a685e6ab836b78b286dd0a234a08ab679698ecb7
samsia251-byte <samsia251@gmail.com>
2026-02-17 22:06:31 +0600

    Add files via upload

---
+++ q12_230102.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    for (int i = 1; i <= 20; i++)
+    {
+        printf("%d\n", i);
+    }
+}
---
+++ q13_230102.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    for (int i = 10; i >= 1; i--)
+    {
+        printf("%d\n", i);
+    }
+}
