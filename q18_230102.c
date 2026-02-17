1fcd304a7b6bb963eee065b78a43c1026dceacdc
samsia251-byte <samsia251@gmail.com>
2026-02-17 22:08:04 +0600

    Add files via upload

---
+++ q18_230102.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    for (int i = 0; i <= 40; i += 4)  
+    {
+        printf("%d\n", i);
+    }
+}
---
+++ q20_230102.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("5 x %d = %d\n", i, 5 * i);
+    }
+}
